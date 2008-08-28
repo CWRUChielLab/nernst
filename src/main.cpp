64860183573f620f23960b5faf3f38102c22f909
Barry Rountree <rountree@eponymous.cs.uga.edu>
2008-08-28 05:43:43 -0400

    Initial multithreading code added.

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,9 @@
 
 
 #include <QApplication>
-
+#include <stdlib.h>
+#include <malloc.h>
+#include "main.h"
 #include "options.h"
 #include "sim.h"
 #include "gui.h"
@@ -37,18 +39,146 @@ main( int argc, char *argv[] )
 
    struct options *o;
    o = parseOptions( argc, argv );
+   fprintf(stderr, "threads = %d\n", o->threads);
 
-   if( o->use_gui )
+   //FIXME multithread gui case.
+   if( o->use_gui && o->threads == 1)
    {
+      //Single-threaded gui.
       app = safeNew( QApplication( argc, argv ) );
       NernstGUI gui( o );
       gui.show();
       return app->exec();
-   } else {
+   } else if (o->threads > 1){
+      //Multi-threaded non-gui.
+	// Create the main GUI thread and a custom event.
+	MainThread *app = safeNew( MainThread( argc, argv ) );	
+	app->o = o;
+	app->LocalInit(app);
+	// Start the thread running.
+	return app->exec();
+   }else{
+      //Single-threaded non-gui.
       app = safeNew( QCoreApplication( argc, argv ) );
       NernstSim sim( o );
       sim.runSim();
       return 0;
    }
+}
+
+/* main.cpp
+ *
+ */
+
+
+#define BLR_NTHREADS 4
+#define BLR_NROUNDS 4
+
+//===========================================================================
+// MainThread
+//===========================================================================
+
+void
+MainThread::customEvent(QEvent *e){
+	// The interesting work has been moved to the various 
+	// subclasses of CustomEvent.
+	((class CustomEvent *)e)->work();
+}
+
+void
+MainThread::LocalInit( class MainThread *app ){
+	int i;
+
+        fprintf(stderr, "threads = %d\n", o->threads);
+	nWorkers = o->threads;
+	
+	// Allocate the arrays for worker threads and messages.
+
+	worker = (class WorkerThread **)
+		 malloc( sizeof(class WorkerThread *) * nWorkers );
+
+	event = (class WorkEvent **)
+		malloc( sizeof(class WorkEvent *) * nWorkers );
+
+	// Fill in the arrays.
+
+	for(i=0; i<nWorkers; i++){
+		worker[i] = safeNew( WorkerThread( i, app ) );
+		event[i]  = safeNew( WorkEvent( i, app, worker[i] ) );
+
+		// Prime the worker message queue.
+		
+		QCoreApplication::postEvent( worker[i], event[i] );
+
+		// Begin the thread w/ an event queue.
+
+		worker[i]->start();
+	}
+}
+
+
+//===========================================================================
+// WorkerThread
+//===========================================================================
+
+void
+WorkerThread::customEvent(QEvent *e){
+	// The interesting work has been moved to the various 
+	// subclasses of CustomEvent.
+	((class CustomEvent *)e)->work();
+}
+
+void
+WorkerThread::run(){
+	exec();
+}
+
+//===========================================================================
+// Several events
+//===========================================================================
+
+void
+WorkEvent::work(){
+	WorkAckEvent *reply = safeNew( WorkAckEvent( id, app, worker ) );
+	fprintf(stdout, "Worker %d received msg.\n", id);
+	QCoreApplication::postEvent( app, reply );
+}
+
+void
+WorkAckEvent::work(){
+	fprintf(stdout, "Main received notice from worker %d.\n", id);
+	QuitEvent *event = safeNew( QuitEvent( id, app, worker ) );
+	QCoreApplication::postEvent( worker, event );
+
+}
+
+void
+QuitEvent::work(){
+	fprintf(stdout, "Worker %d quitting.\n", id);
+	QuitAckEvent *event = safeNew( QuitAckEvent( id, app, worker ) );
+	QCoreApplication::postEvent( worker, event );
+
+	worker->exit(0);
+
+}
+
+void
+QuitAckEvent::work(){
+	worker->wait();
+	fprintf(stdout, "Main received notice worker %d is finished.\n", id);
+	app->nWorkers--;
+	if(! app->nWorkers ){
+		app->exit(0);
+	}
+}
+
+
+//===========================================================================
+// CustomEvent
+//===========================================================================
+
+void
+CustomEvent::work(){
+	fprintf(stdout, "Why is CustomEvent::work() getting called?\n");
 }
 
---
+++ src/main.h
@@ -0,0 +1,133 @@
+/* main.h
+ *
+ */
+#include <QCoreApplication>
+#include <QThread>
+#include <QEvent>
+#include <QSemaphore>
+
+class WorkerThread;
+class MainThread;
+class CustomEvent;
+class NernstSim;
+
+//===========================================================================
+// Parent events
+//===========================================================================
+class CustomEvent : public QEvent {
+	public:
+		CustomEvent():QEvent(QEvent::User){}
+		virtual void work();
+};	
+
+//===========================================================================
+// Events sent by main, received by workers.  Be sure to include app and thread ptrs.
+//===========================================================================
+class WorkEvent : public CustomEvent {
+	public:
+		int id;
+		class MainThread *app;
+		class WorkerThread *worker;
+		WorkEvent( int param_id, MainThread *param_app, WorkerThread *param_worker ) :
+			CustomEvent(), id( param_id ), app(param_app), worker(param_worker){}
+		virtual void work();
+};
+
+class QuitEvent : public CustomEvent {
+	public:
+		int id;
+		class MainThread *app;
+		class WorkerThread *worker;
+		QuitEvent( int param_id, MainThread *param_app, WorkerThread *param_worker ) :
+			CustomEvent(), id( param_id ), app(param_app), worker(param_worker){}
+		virtual void work();
+};
+
+//===========================================================================
+// Events sent by workers, received by main.
+//===========================================================================
+class WorkAckEvent : public CustomEvent {
+	public:
+		int id;
+		class MainThread *app;
+		class WorkerThread *worker;
+		WorkAckEvent( int param_id, MainThread *param_app, WorkerThread *param_worker ) :
+			CustomEvent(), id( param_id ), app(param_app), worker(param_worker){}
+		virtual void work();
+};
+		
+class QuitAckEvent : public CustomEvent {
+	public:
+		int id;
+		class MainThread *app;
+		class WorkerThread *worker;
+		QuitAckEvent( int param_id, MainThread *param_app, WorkerThread *param_worker ) :
+			CustomEvent(), id( param_id ), app(param_app), worker(param_worker){}
+		virtual void work();
+};
+
+
+
+class MainThread : public QCoreApplication {
+	public:
+		//-----------------------------------------------------------
+		// Functions
+		//-----------------------------------------------------------
+		
+		// Subclass QCoreApplication so we can override the
+		// event processing.
+		MainThread( int param_argc, char **param_argv ) :
+			QCoreApplication( param_argc, param_argv ), 
+			argc(param_argc), argv(param_argv){}
+		
+		// Override the custom event queue so we can grab our own 
+		// events.
+		void customEvent( QEvent *e );
+
+		// Initialize here rather in the constructor.  Makes life 
+		// just a tiny bit easier.
+		void LocalInit( class MainThread *app );
+
+		//-----------------------------------------------------------
+		// Variables
+		//-----------------------------------------------------------
+
+		int argc;
+		char **argv;
+		struct options *o;
+
+		// Assume that number of workers will be fetched off of the 
+		// commmand line.
+		int nWorkers;
+
+		// Our worker threads.
+		class WorkerThread **worker;
+
+		// These are the messages we use (they get recycled).
+		class WorkEvent **event;
+	private:
+		NernstSim *s;
+
+	
+};
+
+
+class WorkerThread : public QThread {
+	public:
+		// What this ought to do is:
+		// 1.  Provide the only constructor for class WorkerThread.
+		// 2.  Set param_parent to 0 if it is not provided.
+		// 3.  Pass param_parent along to the parent class (QThread).
+		// 4.  Set this->id = param_id.
+		int id;
+		WorkerThread(int param_id, QObject *param_parent=0) : 
+			QThread( param_parent ), id( param_id ){}
+		
+		// Override the custom event queue so we can grab our own events.
+		void customEvent( QEvent *e );
+
+		// This is a pure virtual function that we have to override.
+		// I think all it needs to do is call exec.
+		virtual void run();
+};
+
